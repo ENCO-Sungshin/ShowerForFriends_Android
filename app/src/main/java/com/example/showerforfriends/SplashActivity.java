177a1d4517947870c0f70ba4cd4610cc8f1dc6eb
Lee Jiwon <jw010208@gmail.com>
2021-07-28 22:13:58 +0900

    [ADD]Before Connect

--- app/src/main/java/com/example/showerforfriends/SplashActivity.java
+++ app/src/main/java/com/example/showerforfriends/SplashActivity.java
@@ -2,15 +2,24 @@ package com.example.showerforfriends;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.core.Amplify;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
+        try {
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
         Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
         startActivity(intent);
     }
